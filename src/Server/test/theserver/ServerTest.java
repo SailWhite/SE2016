7a78d918b3738bc92970ddbced6b95b87dc1ffd6
a123bdef <13307130187@fudan.edu.cn>
2016-06-04 19:40:07 +0800

    update_黑盒测试_Question

--- src/Server/test/theserver/ServerTest.java
+++ src/Server/test/theserver/ServerTest.java
@@ -103,8 +103,53 @@ public class ServerTest {
      */
     @Test
     public void test_2_Question() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+//Add Question 1
+        String debugMessage = process("{\"command\":\"addQuestion\",\"token\":\""+token+"\",\"content\":\"This is Question 1\"}");
+        assertEquals(debugMessage, "{\"result\":\"Success\",\"id\":\"4\"}");
+//Add Question 2
+        debugMessage = process("{\"command\":\"addQuestion\",\"token\":\""+token+"\",\"content\":\"This is Question 2\"}");
+        assertEquals(debugMessage, "{\"result\":\"Success\",\"id\":\"6\"}");
+//Add Question 3 token=null
+        debugMessage = process("{\"command\":\"addQuestion\",\"token\":\"\",\"content\":\"This is Question 3\"}");
+        assertEquals(debugMessage, "{\"result\":\"Incorrect command\"}");
+
+//Get Questions of User 1
+//输出不太对？
+        debugMessage = process("{\"command\":\"getQuestions\",\"token\":\""+token+"\"}");
+        assertEquals(debugMessage, "{\"result\":\"Success\",\"questions\":\"{\\\"author\\\":\\\"{\\\\\\\"id\\\\\\\":\\\\\\\"1\\\\\\\",\\\\\\\"username\\\\\\\":\\\\\\\"sw7\\\\\\\"}\\\",\\\"id\\\":\\\"6\\\",\\\"content\\\":\\\"This is Question 2\\\"}\"}");
+
+//token=null
+        debugMessage = process("{\"command\":\"getQuestions\",\"token\":\"\"}");
+        //assertEquals(debugMessage, "{\"result\":\"Incorrect command\"}");
+
+//Update Question 1
+        debugMessage = process("{\"command\":\"updateQuestion\",\"token\":\""+token+"\",\"content\":\"This is Question 1(Updated)\",\"id\":\"4\"}");
+        assertEquals(debugMessage, "{\"result\":\"Success\"}");
+
+//Mismatched id
+        debugMessage = process("{\"command\":\"updateQuestion\",\"token\":\""+anotherToken+"\",\"content\":\"This is Question 1(Mismatched)\",\"id\":\"4\"}");
+        assertEquals(debugMessage, "{\"result\":\"Failed\"}");
+
+//Invalid id
+        debugMessage = process("{\"command\":\"updateQuestion\",\"token\":\""+token+"\",\"content\":\"This is Question 1(Invalid id)\",\"id\":\"5\"}");
+        assertEquals(debugMessage, "{\"result\":\"Failed\"}");
+        
+//Delete Question 2
+        debugMessage = process("{\"command\":\"deleteQuestion\",\"token\":\""+token+"\",\"id\":\"6\"}");
+        assertEquals(debugMessage, "{\"result\":\"Success\"}");
+
+//Mismatched id       
+        debugMessage = process("{\"command\":\"deleteQuestion\",\"token\":\""+anotherToken+"\",\"id\":\"6\"}");
+        assertEquals(debugMessage, "{\"result\":\"Failed\"}");
+        
+//Delete Again
+        debugMessage = process("{\"command\":\"deleteQuestion\",\"token\":\""+token+"\",\"id\":\"6\"}");
+        assertEquals(debugMessage, "{\"result\":\"Failed\"}");
+//Check result
+        debugMessage = process("{\"command\":\"getQuestions\",\"token\":\""+token+"\"}");
+        assertEquals(debugMessage, "{\"result\":\"Success\",\"questions\":\"{\\\"author\\\":\\\"{\\\\\\\"id\\\\\\\":\\\\\\\"1\\\\\\\",\\\\\\\"username\\\\\\\":\\\\\\\"sw7\\\\\\\"}\\\",\\\"id\\\":\\\"4\\\",\\\"content\\\":\\\"This is Question 1(Updated)\\\"}\"}");
+        
+        //fail("The test case is a prototype.");
     }
     
     /**
